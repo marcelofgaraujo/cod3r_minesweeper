3b98b237e3d0d9b0f22906e6035e1c593240588e
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-01-15 13:56:25 -0300

    method to add near fields

--- src/br/com/cod3r/ms/model/Field.java
+++ src/br/com/cod3r/ms/model/Field.java
@@ -1,5 +1,8 @@
 package br.com.cod3r.ms.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
 	
 	private final int row;
@@ -9,8 +12,30 @@ public class Field {
 	private boolean undermined;
 	private boolean marked;
 	
+	private List<Field> neighbourhood = new ArrayList<>();
+	
 	Field(int row, int column) {
 		this.row = row;
 		this.column = column;
+	}
+	
+	boolean addNeighbour(Field neighbour) {
+		boolean differentRow = this.row != neighbour.row;
+		boolean differentColumn = this.column != neighbour.column;
+		boolean diagonal = differentRow && differentColumn;
+		
+		int deltaRow = Math.abs(this.row - neighbour.row);
+		int deltaColumn = Math.abs(this.column- neighbour.column);
+		int genDelta = deltaRow + deltaColumn;
+		
+		if(genDelta == 1 && !diagonal) {
+			neighbourhood.add(neighbour);
+			return true;
+		} else if(genDelta == 2 && diagonal) {
+			neighbourhood.add(neighbour);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
