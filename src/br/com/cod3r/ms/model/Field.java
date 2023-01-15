74a51203e414d3c0a4d91df4377e2e53a3178532
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-01-15 13:33:52 -0300

    attributes to determine open and marked fields

--- src/br/com/cod3r/ms/model/Field.java
+++ src/br/com/cod3r/ms/model/Field.java
@@ -4,7 +4,10 @@ public class Field {
 	
 	private final int row;
 	private final int column;
+	
+	private boolean open;
 	private boolean undermined;
+	private boolean marked;
 	
 	Field(int row, int column) {
 		this.row = row;
