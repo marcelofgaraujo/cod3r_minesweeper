4097e20da968ca7c7c56a023edfa202a87a31b5e
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-01-20 14:36:12 -0300

    message dialog to show to user when he wins or lose the game

--- src/br/com/cod3r/ms/view/BoardPanel.java
+++ src/br/com/cod3r/ms/view/BoardPanel.java
@@ -2,21 +2,30 @@ package br.com.cod3r.ms.view;
 
 import java.awt.GridLayout;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import br.com.cod3r.ms.model.Board;
 
 public class BoardPanel extends JPanel {
 
 	private static final long serialVersionUID = -1626447743158795895L;
-	
+
 	public BoardPanel(Board board) {
-		
+
 		setLayout(new GridLayout(board.getRows(), board.getColumns()));
-		
+
 		board.forEachField(f -> add(new FieldButton(f)));
-		board.registerObserver(e -> {
-			// TODO show game result
+
+		SwingUtilities.invokeLater(() -> {
+			board.registerObserver(e -> {
+				if (Boolean.FALSE) {
+					JOptionPane.showMessageDialog(this, "You win!! :D");
+				} else {
+					JOptionPane.showMessageDialog(this, "You lose!! :(");
+				}
+			});
 		});
 	}
 
