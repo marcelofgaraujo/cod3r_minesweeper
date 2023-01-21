0c91df10f06404150736641652573eda4dcbfb4e
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-01-21 14:47:40 -0300

    tested generateFields (failure: fields number getting duplicated) and sortMines(success)

---
+++ test/br/com/cod3r/ms/model/BoardTests.java
@@ -0,0 +1,28 @@
+package br.com.cod3r.ms.model;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTests {
+	
+	private int mines = 5;
+	private int rows = 5;
+	private int columns = 5;
+	private Board board = new Board(rows, columns, mines);
+	
+	@Test
+	void testGenerateFields() {
+		board.generateFields();
+		boolean result = board.getFieldsNumber() == rows * columns;
+		assertTrue(result);
+	}
+	
+	@Test
+	void testSortMines() {
+		board.sortMines();
+		boolean result = board.getMines() == mines;
+		assertTrue(result);
+	}
+}
