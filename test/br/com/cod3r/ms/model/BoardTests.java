a13ebcb02901fd3579db3b8d71e2414f6e023155
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-01-22 12:11:14 -0300

    testing open fields method

--- test/br/com/cod3r/ms/model/BoardTests.java
+++ test/br/com/cod3r/ms/model/BoardTests.java
@@ -1,35 +1,41 @@
 package br.com.cod3r.ms.model;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
 public class BoardTests {
-	
+
 	private int mines = 5;
 	private int rows = 5;
 	private int columns = 5;
 	private Board board = new Board(rows, columns, mines);
 	private List<Field> fields = board.getFields();
-	
+
 	@Test
 	void testGenerateFields() {
 		boolean result = fields.size() == rows * columns;
 		assertTrue(result);
 	}
-	
+
 	@Test
 	void testSortMines() {
 		boolean result = board.getMines() == mines;
 		assertTrue(result);
 	}
-	
+
 	@Test
 	void testOpenField() {
+		board.openField(4, 3);
+		boolean result = fields
+			  .parallelStream()
+			  .filter(f -> f.isOpen() && (f.getRow() == 4 && f.getColumn() == 3))
+			  .findFirst()
+			  .isPresent();
 		
-		
+		assertTrue(result);
 	}
 }
